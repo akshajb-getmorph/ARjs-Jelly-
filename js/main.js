7015a29db43da2832ded717a41da778d99c379f4
Akshaj Balaji <akshajb@Akshajs-MacBook.local>
2019-08-28 19:41:37 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    
+    $('.splash').click(function(){
+        $(this).fadeOut(500);
+        $('.addJelly').fadeIn(500);
+    })
+    $('.addJelly').click(function(){
+        $(this).fadeOut(500);
+        var scene = document.querySelector('a-scene');
+        let jelly = scene.querySelector('#jelly');
+        let anim = document.createElement('a-animation');
+        let animJ = $('a-animation');
+        anim.setAttribute('attribute',"position");
+        anim.setAttribute('dur',"1500");
+        anim.setAttribute('to','2 0.80 -1.85');
+        anim.setAttribute('easing','linear');
+        jelly.appendChild(anim);
+        console.log(jelly);
+    });
+});
