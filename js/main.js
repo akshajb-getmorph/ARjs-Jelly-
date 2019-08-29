2b1ce3a8c6c51a2b5779ce1f177da766132857ab
Akshaj Balaji <akshajb@Akshajs-MacBook.local>
2019-08-29 10:27:49 +0530

    remove animation

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@ $( document ).ready(function() {
     
     $('.splash').click(function(){
         $(this).fadeOut(500);
+        var scene = document.querySelector('a-scene');
+        let cake = scene.querySelector('#cake');
+        cake.setAttribute("visible",true);
         $('.addJelly').fadeIn(500);
     })
     $('.addJelly').click(function(){
         $(this).fadeOut(500);
         var scene = document.querySelector('a-scene');
         let jelly = scene.querySelector('#jelly');
-        let anim = document.createElement('a-animation');
-        let animJ = $('a-animation');
         jelly.setAttribute("visible",true);
-        anim.setAttribute('attribute',"position");
-        anim.setAttribute('dur',"1500");
-        anim.setAttribute('to','2 0.80 -1.85');
-        anim.setAttribute('easing','linear');
-        jelly.appendChild(anim);
+        // let animJ = $('a-animation');
+        // let anim = document.createElement('a-animation');
+        // anim.setAttribute('attribute',"position");
+        // anim.setAttribute('dur',"1500");
+        // anim.setAttribute('to','2 0.80 -1.85');
+        // anim.setAttribute('easing','linear');
+        // jelly.appendChild(anim);
         console.log(jelly);
     });
 });
